9a2349bcc4ea3c09c0db11349daf632a286831a4
Jim Szalewski <jim.szalewski@gmail.com>
2017-09-14 21:17:31 -0600

    added successful flash message to account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
       redirect_to new_confirmation_path
     else
       render :new
+      flash[:error] = "Please re-enter your information."
     end
   end
 
